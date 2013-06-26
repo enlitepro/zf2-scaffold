e82f5c0ac685597fd449b6bf806a9f00b0f6fc5e
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-06-26 17:34:10 +0300

    Binaries

--- bin/scaffold.php
+++ bin/scaffold.php
@@ -5,7 +5,13 @@
 use Scaffold\Console\EntityCommand;
 use Symfony\Component\Console\Application;
 
-require __DIR__ . "/../vendor/autoload.php";
+if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
+    require __DIR__ . "/../vendor/autoload.php";
+} elseif (file_exists(__DIR__ . "/../autoload.php")) {
+    require __DIR__ . "/../autoload.php";
+} else {
+    throw new \RuntimeException('Where autoload, buddy?');
+}
 
 $application = new Application();
 $application->add(new EntityCommand());
