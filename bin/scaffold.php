924dcc8d10dd5d1f42fd3c4bcc876efbd57a2372
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-06-26 17:35:41 +0300

    Binary autoload

--- bin/scaffold.php
+++ bin/scaffold.php
@@ -7,8 +7,8 @@ use Symfony\Component\Console\Application;
 
 if (file_exists(__DIR__ . "/../vendor/autoload.php")) {
     require __DIR__ . "/../vendor/autoload.php";
-} elseif (file_exists(__DIR__ . "/../autoload.php")) {
-    require __DIR__ . "/../autoload.php";
+} elseif (file_exists(__DIR__ . "/../../../autoload.php")) {
+    require __DIR__ . "/../../../autoload.php";
 } else {
     throw new \RuntimeException('Where autoload, buddy?');
 }
