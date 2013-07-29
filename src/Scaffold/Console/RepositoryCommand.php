983f1a58123b75940d4ef4994f96e4a0abff2e2d
Evgeny Shpilevsky <zhekanot@gmail.com>
2013-07-29 14:56:46 +0300

    Repository options

--- src/Scaffold/Console/RepositoryCommand.php
+++ src/Scaffold/Console/RepositoryCommand.php
@@ -9,6 +9,7 @@ namespace Scaffold\Console;
 use Scaffold\State;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class RepositoryCommand extends AbstractCommand
@@ -19,13 +20,21 @@ class RepositoryCommand extends AbstractCommand
         $this->setDescription('Create controller');
         $this->addArgument('module', InputArgument::REQUIRED, 'Module name');
         $this->addArgument('name', InputArgument::REQUIRED, 'Repository name');
+        $this->addOption('no-trait', null, InputOption::VALUE_NONE, 'Generate without trait');
+        $this->addOption('no-repository', null, InputOption::VALUE_NONE, 'Generate without repository');
     }
 
     protected function write(State $state, InputInterface $input, OutputInterface $output)
     {
         $writeState = new State($this->configWriter);
-        $writeState->addModel($state->getRepositoryModel());
-        $writeState->addModel($state->getModel('repository-trait'));
+
+        if (!$input->getOption('no-repository')) {
+            $writeState->addModel($state->getRepositoryModel());
+        }
+
+        if (!$input->getOption('no-trait')) {
+            $writeState->addModel($state->getModel('repository-trait'));
+        }
 
         parent::write($writeState, $input, $output);
     }
